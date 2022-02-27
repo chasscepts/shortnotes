79c395cbb775c19697d3b3377fa5649dbd228185
Obetta Francis <francischassobetta@gmai.com>
2022-02-27 08:16:50 +0100

    Implement Noteviewer

--- src/components/NoteViewer/index.js
+++ src/components/NoteViewer/index.js
@@ -1,9 +1,38 @@
+import { useSelector } from '../../redux';
+import { selectNote } from '../../app/reducers/slice';
 import css from './style.module.css';
+import ErrorPage from '../ErrorPage';
+import LoadingSpinner from '../LoadingSpinner';
+import 'highlight.js/styles/github.css';
+import './highlight.css';
 
-const NoteViewer = () => (
-  <div className={css.container}>
-    <h1>Welcome</h1>
-  </div>
-);
+const NoteViewer = () => {
+  const { item: note, loading, error } = useSelector(selectNote);
+
+  if (error) {
+    return <ErrorPage error={error} />;
+  }
+
+  if (loading) {
+    return <LoadingSpinner message="Loading Note ..." />;
+  }
+
+  if (!note) {
+    return (
+      <section className={css.noItem}>
+        <p>Nothing To Display</p>
+      </section>
+    );
+  }
+
+  return (
+    <section className={css.container}>
+      <header className={css.header}>
+        <h2>{note.title}</h2>
+      </header>
+      <div className={css.viewer}>{note.content}</div>
+    </section>
+  );
+};
 
 export default NoteViewer;
--- src/utilities/mark.js
+++ src/utilities/mark.js
@@ -1,3 +1,21 @@
-const mark = (text) => text;
+import { marked } from 'marked';
+import hljs from 'highlight.js';
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  highlight: (code, language) => {
+    const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
+    return hljs.highlight(code, { language: validLanguage }).value;
+  },
+  pedantic: false,
+  gfm: true,
+  breaks: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false,
+});
+
+const mark = (text) => marked(text);
 
 export default mark;
